9e051ef311319379229f6f8b6a9129c8ed52fa9a
zemdam <az430277@students.mimuw.edu.pl>
2023-12-07 14:04:31 +0100

    Add global cors

--- src/main/java/pl/mimuw/zpp/quantumai/backendui/BackendUiApplication.java
+++ src/main/java/pl/mimuw/zpp/quantumai/backendui/BackendUiApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -18,5 +20,15 @@ public class BackendUiApplication {
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
     }
+
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*"); // Later we can specify allowed origins.
+			}
+		};
+	}
 
 }
