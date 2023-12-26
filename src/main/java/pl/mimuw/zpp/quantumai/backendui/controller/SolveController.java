e289d56ae68e99049de828b471e448c54c9d10d2
JKuziola <jakub.kuziola1@gmail.com>
2023-12-26 14:20:39 +0100

    New endpoint that takes file instead of string to solve TSP.

--- src/main/java/pl/mimuw/zpp/quantumai/backendui/controller/SolveController.java
+++ src/main/java/pl/mimuw/zpp/quantumai/backendui/controller/SolveController.java
@@ -3,6 +3,9 @@ package pl.mimuw.zpp.quantumai.backendui.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.vavr.control.Either;
 import lombok.RequiredArgsConstructor;
+
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.mimuw.zpp.quantumai.backendui.model.TspResult;
@@ -27,8 +30,28 @@ public class SolveController {
         }
     }
 
+    // Similar to the above, but the input is a file
+    @PostMapping("/tsp/file")
+    public ResponseEntity<String> solveTspWithFile(
+            @ModelAttribute SolveInputFile solveInputFile
+    ) throws Exception {
+        String pythonCode = new String(solveInputFile.pythonFile().getBytes(), StandardCharsets.UTF_8);
+        Either<String, TspResult> result = solveService.solveTsp(pythonCode, solveInputFile.graphName());
+        if (result.isRight()) {
+            return ResponseEntity.ok(mapper.writeValueAsString(result.get()));
+        } else {
+            return ResponseEntity.badRequest().body(result.getLeft());
+        }
+    }
+
     record SolveInput(
        String pythonCode,
        String graphName
     ) {}
+
+    record SolveInputFile(
+        MultipartFile pythonFile,
+        String graphName
+    ) {}
+
 }
