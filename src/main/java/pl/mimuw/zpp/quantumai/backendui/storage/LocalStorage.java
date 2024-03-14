6d04742e732ecd2b2e1bfd51f048f22517bf5545
Wiktor Chmielewski <chmielewski.wiktor@gmail.com>
2024-03-14 10:50:54 +0100

    Now using Mongo as Storage instead of local filesystem

--- src/main/java/pl/mimuw/zpp/quantumai/backendui/storage/LocalStorage.java
+++ src/main/java/pl/mimuw/zpp/quantumai/backendui/storage/LocalStorage.java
@@ -1,6 +1,6 @@
 package pl.mimuw.zpp.quantumai.backendui.storage;
 
-import org.springframework.context.annotation.Primary;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,18 +9,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 @Component
-@Primary
+@ConditionalOnMissingBean(MongoStorage.class)
 public class LocalStorage implements Storage {
     @Override
-    public String save(MultipartFile multipartFile, String path) throws IOException {
-        File directory = Files.createTempDirectory(path).toFile();
+    public String save(MultipartFile multipartFile) throws IOException {
+        File directory = Files.createTempDirectory(null).toFile();
         File file = File.createTempFile("solve", null, directory);
         multipartFile.transferTo(file);
         return file.getAbsolutePath();
     }
 
     @Override
-    public File get(String path) {
+    public File get(String path) throws IOException {
         return new File(path);
     }
 }
