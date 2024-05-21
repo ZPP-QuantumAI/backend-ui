4c90991baf88e7f6cdc748d6578651397bbf7c38
Wiktor Chmielewski <chmielewski.wiktor@gmail.com>
2024-05-21 18:42:35 +0200

    Removed unnecessary filename

--- src/main/java/pl/mimuw/zpp/quantumai/backendui/storage/MongoStorage.java
+++ src/main/java/pl/mimuw/zpp/quantumai/backendui/storage/MongoStorage.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
-import pl.mimuw.zpp.quantumai.backendui.utils.RandomNameGenerator;
 
 import java.io.IOException;
 
@@ -13,11 +12,9 @@ import java.io.IOException;
 @RequiredArgsConstructor
 @Slf4j
 public class MongoStorage implements Storage {
-    private final RandomNameGenerator randomNameGenerator;
     private final GridFsTemplate gridFsTemplate;
     @Override
     public String save(MultipartFile multipartFile) throws IOException {
-        String id = randomNameGenerator.generateName();
-        return gridFsTemplate.store(multipartFile.getInputStream(), id, "application/zip").toString();
+        return gridFsTemplate.store(multipartFile.getInputStream(), null, "application/zip").toString();
     }
 }
