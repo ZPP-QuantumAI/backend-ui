cdf4ce28fe90a8538cec9f9e35b93d2937883106
wiktoor <74465264+wiktoor@users.noreply.github.com>
2024-05-21 18:31:57 +0200

    Added support for grid-fs (#11)

--- src/main/java/pl/mimuw/zpp/quantumai/backendui/repository/FileRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.mimuw.zpp.quantumai.backendui.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import pl.mimuw.zpp.quantumai.backendui.model.SolutionFile;
-
-@Repository
-public interface FileRepository extends MongoRepository<SolutionFile, String>{
-}
--- src/main/java/pl/mimuw/zpp/quantumai/backendui/storage/Storage.java
+++ src/main/java/pl/mimuw/zpp/quantumai/backendui/storage/Storage.java
@@ -2,11 +2,8 @@ package pl.mimuw.zpp.quantumai.backendui.storage;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 
 public interface Storage {
     String save(MultipartFile multipartFile) throws IOException;
-
-    File get(String id) throws IOException;
 }
